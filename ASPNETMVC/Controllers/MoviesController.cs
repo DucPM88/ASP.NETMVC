898a60fda574d9074c9ceb08d5537619c52f684c
Phan Đức <35461758+PhanMinhDuck61T@users.noreply.github.com>
2019-08-07 23:45:11 +0700

    Section 1+ 2

---
+++ ASPNETMVC/Controllers/MoviesController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ASPNETMVC.Models;
+using ASPNETMVC.ViewModels;
+
+namespace ASPNETMVC.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shark" };
+            ViewData["Movies"] = movie;
+            ViewBag.Movie = movie;
+            var customer = new List<Customer>
+            {
+                new Customer{Name = "Customer 1"},
+                new Customer{Name = "Customer 2"}
+            };
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customer
+            };
+            return View(viewModel);
+            //return Content("Hello");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "Name"});
+            
+
+        }
+        public ActionResult Edit(int id)
+        {
+            return Content("id = " + id);
+        }
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "name";
+            return Content(String.Format("pageIndex = {0}&sortBy={1}", pageIndex, sortBy));
+        }
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+    }
+}
